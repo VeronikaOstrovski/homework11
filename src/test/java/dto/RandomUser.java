d0429e795c3e8925133d3b3b7c3eca9f6587c0dd
veronikaostrovski <veronika.turkey@gmail.com>
2023-07-01 02:27:43 +0300

    Task done

--- src/test/java/dto/RandomUser.java
+++ src/test/java/dto/RandomUser.java
@@ -1,2 +1,63 @@
-package dto;public class RandomUser {
+package dto;
+
+public class RandomUser {
+    int id;
+    String username;
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+    String phone;
+    int userStatus;
+
+    public RandomUser(int id, String username, String firstName, String lastName, String email, String password, String phone, int userStatus) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.userStatus = userStatus;
+    }
+
+    public RandomUser() {
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserStatus(int userStatus) {
+
+        this.userStatus = userStatus;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
--- src/test/java/org/example/CreatedUser.java
+++ src/test/java/org/example/CreatedUser.java
@@ -1,2 +1,77 @@
-package org.example;public class Creta {
+package org.example;
+
+import com.google.gson.Gson;
+import dto.RandomUser;
+import io.restassured.response.Response;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static io.restassured.RestAssured.given;
+import static org.apache.commons.lang3.RandomStringUtils.random;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+
+public class CreatedUser {
+    @Test
+    public void toCreateUser() {
+        RandomUser randomUser = new RandomUser(25, "test", "Veronika", "Ost", "test@gmail.com", "1234567", "+37212345678", 12);
+        RandomUser randomUserEmpty = new RandomUser();
+        randomUser.setId(25);
+        randomUser.setUsername("test");
+        randomUser.setFirstName("Veronika");
+        randomUser.setLastName("Ost");
+        randomUser.setEmail("test@gmail.com");
+        randomUser.setPassword("12345678");
+        randomUser.setPhone("+37212345678");
+        randomUser.setUserStatus(12);
+        randomUserEmpty.setId(25);
+        randomUserEmpty.setUsername("test1");
+        randomUserEmpty.setFirstName(generatedRandomFirstName());
+        randomUserEmpty.setLastName("Ostrov");
+        randomUserEmpty.setEmail("post@gmail.com");
+        randomUserEmpty.setPassword("7654321");
+        randomUserEmpty.setPhone(generatedRandomFirstName());
+        randomUserEmpty.setUserStatus(12);
+
+        Gson gson = new Gson();
+        given()
+                .when()
+                .header("Content-Type", "application/json")
+                .body(gson.toJson(randomUserEmpty))
+                .log()
+                .all()
+                .post("https://petstore.swagger.io/v2/user")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK);
+
+        Assertions.assertEquals( 25, randomUser.getId());
+
+    }
+
+    public String generatedRandomFirstName() {
+        RandomStringUtils randomStringUtils = new RandomStringUtils();
+        int lengthFirstName = 10;
+        boolean useLettersFirstName = false;
+        boolean useSymbolsFirstName = true;
+        String generatedStringFirstName = RandomStringUtils.random(lengthFirstName, useLettersFirstName, useSymbolsFirstName);
+
+             return generatedStringFirstName;
+    }
+
+    public String generatedRandomPhone() {
+        RandomStringUtils randomStringUtils = new RandomStringUtils();
+        int lengthFirstName = 15;
+        boolean useLettersPhone = false;
+        boolean useNumbersPhone = true;
+        String generatedStringPhone = RandomStringUtils.random(lengthFirstName, useLettersPhone, useNumbersPhone);
+
+        return generatedStringPhone;
+    }
 }
+
